f030adf7b054f57266343e0570905af4a15b65e4
Domingo Lopez <dominlopez78@gmail.com>
2018-02-23 15:29:21 +0100

    Changed regularize to iterative_blur

--- R/find_transform.R
+++ R/find_transform.R
@@ -74,7 +74,7 @@ compute_deformation_field <- function(source_coords,
                                       target_coords,
                                       target_dims,
                                       method = c("interpolation", "tps", "mixture"),
-                                      kernel = c()) {
+                                      kernel_size = 0) {
 
   displ <- target_coords - source_coords
 
@@ -115,9 +115,9 @@ compute_deformation_field <- function(source_coords,
 
     Dx2 <- Dx1[seq(target_dims[1]), seq(target_dims[2]), seq(target_dims[3])]
 
-    if (length(kernel) > 0) {
+    if (kernel_size > 0) {
 
-      Dx_norm <- regularize(image = Dx2, kernel = kernel, ncores = parallel::detectCores() - 1)
+      Dx_norm <- iterative_blur(image = Dx2, kernel_size = kernel_size, ncores = parallel::detectCores() - 1)
 
     } else Dx_norm <- Dx2
 
@@ -131,9 +131,9 @@ compute_deformation_field <- function(source_coords,
 
     Dy2 <- Dy1[seq(target_dims[1]), seq(target_dims[2]), seq(target_dims[3])]
 
-    if (length(kernel) > 0) {
+    if (kernel_size > 0) {
 
-      Dy_norm <- regularize(image = Dy2, kernel = kernel, ncores = parallel::detectCores() - 1)
+      Dy_norm <- iterative_blur(image = Dy2, kernel_size = kernel_size, ncores = parallel::detectCores() - 1)
 
     } else Dy_norm <- Dy2
 
@@ -146,9 +146,9 @@ compute_deformation_field <- function(source_coords,
 
     Dz2 <- Dz1[seq(target_dims[1]), seq(target_dims[2]), seq(target_dims[3])]
 
-    if (length(kernel) > 0) {
+    if (kernel_size > 0) {
 
-      Dz_norm <- regularize(image = Dz2, kernel = kernel, ncores = parallel::detectCores() - 1)
+      Dz_norm <- iterative_blur(image = Dz2, kernel_size = kernel_size, ncores = parallel::detectCores() - 1)
 
     } else Dz_norm <- Dz2
 
--- R/iterative_blur.R
+++ R/iterative_blur.R
@@ -1,10 +1,11 @@
-iterative_blur <- function(A, sigma, w = 3) {
+iterative_blur <- function(image, sigma, kernel_size = 3, ncores = parallel::detectCores() - 1) {
 
   n <- 3 * round(sigma)
-  kernel <- gaussian_kernel(sigma = sigma, size = w)
+  kernel <- gaussian_kernel(sigma = sigma, size = kernel_size)
   # kernel <- array(1 / (w ^ 3), dim = c(w, w, w))
 
-  for (i in seq(n)) A <- regularize(A, kernel)
+  A <- image
+  for (i in seq(n)) A <- regularize(A, kernel, ncores = ncores)
 
   return(A)
 

6580260f7103d72ae3e36b10d992ca3632366047
Domingo Lopez <neuroimaginador@gmail.com>
2018-11-20 20:16:05 +0100

    Added function to register several templates to a single subject image, as in malf

---
+++ R/register_malf.R
@@ -0,0 +1,44 @@
+register_malf <- function(image, template_files, label_files) {
+
+  num_subjects <- length(template_files)
+
+  system.time({
+
+    templates <- array(0, dim = c(dim(image), num_subjects))
+    labels <- array(0, dim = c(dim(image), num_subjects))
+
+    if (!ANTsRCore::is.antsImage(image)) {
+
+      img <- ANTsRCore::as.antsImage(image)
+
+    } else {
+
+     img <- image
+
+    }
+
+    for (i in seq(num_subjects)) {
+
+      cat("Registering subject", i, "\n")
+
+      tx <- ANTsRCore::antsRegistration(fixed = img,
+                             moving = ANTsRCore::antsImageRead(template_files[i]),
+                             verbose = FALSE,
+                             typeofTransform = "AffineFast")
+
+      templates[, , , i] <- tx$warpedmovout %>% as.array()
+
+      labels[, , , i] <- ANTsRCore::antsApplyTransforms(fixed = img,
+                                             moving = ANTsRCore::antsImageRead(label_files[i]),
+                                             transformlist = tx$fwdtransforms,
+                                             interpolator = "nearestNeighbor",
+                                             verbose = FALSE) %>%
+        as.array()
+
+    }
+
+  })
+
+  return(list(templates = templates, labels = labels))
+
+}
